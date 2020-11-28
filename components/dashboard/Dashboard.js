453a3c79b2c78e308f2b68322fa68f1ce5a85f07
Jonathan Forney <jonathan.forney@gmail.com>
2020-11-27 18:08:47 -0800

    cleaning up file

--- components/dashboard/Dashboard.js
+++ components/dashboard/Dashboard.js
@@ -2,17 +2,6 @@ import React from 'react';
 import {View, Text} from 'react-native';
 
 const Dashboard = (props) => {
-  let countDownTimeInSeconds =
-    props.timerStateToDash.lengthOfRun - props.timerStateToDash.timerTime;
-
-  let appTimerCurrentTime = {
-    seconds:
-      0 || ('0' + (Math.floor(countDownTimeInSeconds / 1000) % 60)).slice(-2),
-    minutes:
-      0 || ('0' + (Math.floor(countDownTimeInSeconds / 60000) % 60)).slice(-2),
-    hours: 0 || ('0' + Math.floor(countDownTimeInSeconds / 3600000)).slice(-2),
-  };
-
   let formatRunLength = {
     seconds:
       0 ||
